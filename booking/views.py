cf48a59d278868c4fcbfa69889941ec295a2743a
Artem <visor517@yandex.ru>
2023-10-11 01:13:02 +0300

    first template

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Reservation, Room
+
+
+class ReservationListView(ListView):
+    model = Reservation
+    template_name = "index.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context.update({
+            'rooms': Room.objects.all(),
+        })
+        return context
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
+from booking.views import ReservationListView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ReservationListView.as_view(), name="index"),
 ]
