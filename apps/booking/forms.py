ad1ac85b9119c9ecc61062dfd88775912a4168a4
Artem <visor517@yandex.ru>
2023-10-15 15:41:53 +0300

    timetable switching

--- apps/booking/forms.py
+++ apps/booking/forms.py
@@ -1,16 +1,30 @@
 from django import forms
 
-from apps.booking.models import Reservation
+from apps.booking.models import Reservation, Room
 
 
 class ReservationForm(forms.ModelForm):
-    start_time = forms.DateTimeField(widget=forms.DateTimeInput(attrs={
-        "type": "datetime-local", "class": "form-control"}))
-    finish_time = forms.DateTimeField(widget=forms.DateTimeInput(attrs={
-        "type": "datetime-local", "class": "form-control"}))
-    description = forms.CharField(widget=forms.TextInput(attrs={
-        "class": "form-control", "placeholder": "Введите описание"}))
+    start_time = forms.DateTimeField(widget=forms.DateTimeInput(
+        attrs={"type": "datetime-local", "class": "form-control"}))
+    finish_time = forms.DateTimeField(widget=forms.DateTimeInput(
+        attrs={"type": "datetime-local", "class": "form-control"}))
+    description = forms.CharField(widget=forms.TextInput(
+        attrs={"class": "form-control", "placeholder": "Введите описание"}))
 
     class Meta:
         model = Reservation
         fields = "__all__"
+
+
+class TimeTableForm(forms.Form):
+    room_id = forms.ChoiceField(
+        label="Название комнаты",
+        required=False,
+        widget=forms.Select(attrs={"class": "form-control"}),
+        choices=[(room.id, room.name) for room in Room.objects.all()],
+    )
+    table_day = forms.DateField(
+        label="День",
+        required=False,
+        widget=forms.DateInput(attrs={"type": "date", "class": "form-control"}),
+    )
--- apps/booking/views.py
+++ apps/booking/views.py
@@ -1,10 +1,12 @@
+from datetime import date
+
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, ListView
 
 from apps.booking.models import Reservation, Room
-from apps.booking.forms import ReservationForm
+from apps.booking.forms import ReservationForm, TimeTableForm
 
 
 class ReservationListView(ListView):
@@ -12,19 +14,32 @@ class ReservationListView(ListView):
     template_name = "timetable.html"
 
     def get_context_data(self, *args, **kwargs):
+        params = self.request.GET
+        if not (table_day := params.get("table_day")):
+            table_day = date.today().strftime("%Y-%m-%d")
+
+        room_id = params.get("room_id", 1)
+        room = Room.objects.get(id=room_id)
+
         context = super().get_context_data(*args, **kwargs)
         context.update({
-            "rooms": Room.objects.all(),
+            "time_table_form": TimeTableForm(initial={
+                "room_id": room.id,
+                "table_day": table_day}),
             "add_form": ReservationForm,
-            "room": Room.objects.get(id=self.request.GET.get("room-id", 1))
+            "room": room,
         })
         return context
 
     def get_queryset(self):
-        room_id = self.request.GET.get("room-id", 1)
+        params = self.request.GET
+
+        if not (table_day := params.get("table_day")):
+            table_day = date.today().strftime("%Y-%m-%d")
+        room_id = params.get("room_id", 1)
 
         try:
-            return Reservation.objects.filter(room_id=room_id)
+            return Reservation.objects.filter(room_id=room_id, start_time__date=table_day)
         except ObjectDoesNotExist:
             return None
 
